27c0325bacbc65e8e8c1f554f47b8cbcdab6a6d4
ahwaboin3 <56944017+ahwaboin3@users.noreply.github.com>
2025-05-09 11:39:27 +0900

    v34v364v63

--- JavaBasic/c07/autocast/Cat.java
+++ JavaBasic/c07/autocast/Cat.java
@@ -6,6 +6,10 @@ public class Cat extends Animal{
 	public void sound() {
 		System.out.println("야옹~");
 	}
+	
+	public void getFeature() {
+		System.out.println("높은 곳을 좋아합니다.");
+	}
 
 	public static void main(String[] args) {
 		Animal a1=new Cat();
@@ -15,6 +19,14 @@ public class Cat extends Animal{
 		
 		Cat cat=new Cat();
 		Animal animal=cat;
+		
+		//부모 타입으로 자동 타입 변환된 이후에는 부모 클래스에
+		//선언된 필드와 메소드만 접근이 가능합니다.
+		Animal a2=new Cat();
+		//a2.getFeature();(X)
+		//메소드가 자식 클래스에서 재정의되었다면 자식 클래스의
+		//메소드가 대신 호출됩니다.
+		a2.sound();
 	}
 }
 
---
+++ JavaBasic/c07/autocast/book/Book.java
@@ -0,0 +1,20 @@
+package c07.autocast.book;
+
+public class Book {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [name=" + name + "]";
+	}
+
+}
